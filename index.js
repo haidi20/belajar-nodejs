b36d84a59c51cf08997c9c9351d07f9cb2ed1a56
haidi20 <nhaidii@yahoo.com>
2020-06-17 22:08:48 +0800

    cara send format json

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+const members = [
+    {
+        id: 1,
+        name: 'nata',
+        status: 'active',
+    },
+    {
+        id: 2,
+        name: 'andi',
+        status: 'inactive',
+    },
+    {
+        id: 3,
+        name: 'risky',
+        status: 'active',
+    },
+]
+
+app.get('/api/members', (req, res) => res.json(members));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
